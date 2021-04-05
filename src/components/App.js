6b519ba5b29268d775970670efcd11227a988495
lequocdatfit <18120305@student.hcmus.edu.vn>
2021-04-05 19:17:55 +0700

    youtube video

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,58 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import youtube from '../apis/youtube';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-    state = {
-        videos: []
-    }
+  state = {
+    videos: [],
+    selectedVideo: null
+  }
 
-    onSearchInputSubmit = async term => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        });
+  componentDidMount() {
+    this.onSearchInputSubmit('blackpink');
+  }
 
-        this.setState({ videos: response.data.items});
-    }
+  onVideoSelect = video => {
+    this.setState({
+      selectedVideo: video
+    })
+  }
 
-    render() {
-        return(
-            <div className="App ui container">
-                <SearchBar onFormSubmit={this.onSearchInputSubmit} />
-                
-                <VideoList videos={this.state.videos} />
+  onSearchInputSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App ui container">
+        <SearchBar onFormSubmit={this.onSearchInputSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
             </div>
-        )
-    }
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
-    const renderedList = videos.map(video => {
-        return <VideoItem video={video}/>
-    })
-    return(
-        <div className="VideoList ui list">
-            {renderedList}
-        </div>
-    )
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem
+      key={video.id.videoId}
+      onVideoSelect={onVideoSelect}
+      video={video}
+    />
+  })
+  return (
+    <div className="VideoList ui list">
+      {renderedList}
+    </div>
+  )
 }
 
 export default VideoList;
